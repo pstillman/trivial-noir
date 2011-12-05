655baf4bd653c5fb9f27705eb5b5d12cbfea7c1e
J Irving <jonathan.irving@gmail.com>
2011-12-05 15:43:55 -0500

    Add the main method back so lein run works. Add an explicit dependency for clojure-contrib, which has been dropped for clojure 1.3, but lein-ring appears to be dependent upon.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
                            [congomongo "0.1.7"]]
-            :dev-dependencies [[lein-ring "0.4.6"]]
+            :dev-dependencies [[lein-ring "0.4.6"]
+                               ;; needed for lein-ring apparently
+                               [org.clojure/clojure-contrib "1.2.0"]]
             :ring {:handler trivial-noir.server/handler}
             :main trivial-noir.server)
 
--- src/trivial_noir/server.clj
+++ src/trivial_noir/server.clj
@@ -6,9 +6,8 @@
 (def handler (server/gen-handler {:mode :dev
                                   :ns 'trivial-noir}))
 
-;; (defn -main [& m]
-;;   (let [mode (keyword (or (first m) :dev))
-;;         port (Integer. (get (System/getenv) "PORT" "8080"))]
-;;     (server/start port {:mode mode
-;;                         :ns 'trivial-noir})))
-
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8888"))]
+    (server/start port {:mode mode
+                        :ns 'trivial-noir})))
